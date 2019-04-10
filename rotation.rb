31660e360c9b027c3c48d08a1cbbb1d35fe8774a
Mollie Stein <mastein109@gmail.com>
2019-04-09 19:52:12 -0500

    add rotation-point problem

---
+++ rotation.rb
@@ -0,0 +1,22 @@
+  # array that includes an alphabetical list that starts halfway through the alphabet until the end and then starts from the beginning
+  words = [
+    'ptolemaic',
+    'retrograde',
+    'supplant',
+    'undulate',
+    'xenoepist',
+    'asymptote',  # <-- rotates here!
+    'babka',
+    'banoffee',
+    'engender',
+    'karpatka',
+    'othellolagkage',
+]
+
+# use binary search to start at the middle of the array to figure out which way to move
+def reorder_array
+
+   
+
+
+end
